4902934d535fb0b3f2558017cfd020f56e6b0a83
Ubuntu <ubuntu@ip-172-31-81-244.ec2.internal>
2022-02-11 05:39:34 +0000

    add resnet

--- mergesort/mergesort.py
+++ mergesort/mergesort.py
@@ -1,11 +1,12 @@
 import numpy as np
 
 def lambda_handler(event, context):
-    a = np.random.rand(1024*1024*16,)
+    
+    a = np.random.rand(1024*event['size'],)
     for i in range(1000):
         a.sort(kind='mergesort')
     return a[0]
 
     
     
-print(lambda_handler({},{}))
+# print(lambda_handler({},{}))
